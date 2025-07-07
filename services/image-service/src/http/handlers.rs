2cbe183d8c9b9e3d27ff9d072e38cf206673dd40
Takis Kakalis <80459599+Takaros999@users.noreply.github.com>
2025-07-07 14:21:27 -0700

    Delete old structure

--- services/image-service/src/http/handlers.rs
+++
@@ -1,8 +0,0 @@
-use axum::Json;
-use serde_json::{json, Value};
-
-pub async fn hello() -> Json<Value> {
-    Json(json!({
-        "message": "Hello from Image Service"
-    }))
-}
--- services/image-service/src/http/mod.rs
+++
@@ -1,8 +0,0 @@
-mod handlers;
-
-use axum::{routing::get, Router};
-
-/// Creates the routes for this service
-pub fn routes() -> Router {
-    Router::new().route("/hello", get(handlers::hello))
-}
