7fea4fa80723689fc30a91e0431e1d009fc09df9
Takis Kakalis <80459599+Takaros999@users.noreply.github.com>
2025-07-15 02:32:15 +0300

    Update seed script

--- aws-seed.sh
+++ aws-seed.sh
@@ -11,7 +11,7 @@ awslocal dynamodb create-table \
     --key-schema \
         AttributeName=hmac,KeyType=HASH \
     --global-secondary-indexes \
-        'IndexName=topic-index,Keys=[{AttributeName=topic,KeyType=HASH}],Projection={ProjectionType=ALL},BillingMode=PAY_PER_REQUEST' \
+        'IndexName=topic-index,KeySchema=[{AttributeName=topic,KeyType=HASH}],Projection={ProjectionType=ALL}' \
     --billing-mode PAY_PER_REQUEST
 
 # Enable TTL on the push subscriptions table
@@ -19,7 +19,7 @@ awslocal dynamodb update-time-to-live \
     --table-name world-chat-push-subscriptions \
     --time-to-live-specification "Enabled=true,AttributeName=ttl"
 
-awslocal sqs create-queue --queue-name world-chat-notification-queue --attributes '{"FifoQueue": "true"}'
-awslocal sqs create-queue --queue-name world-chat-subscription-request-queue --attributes '{"FifoQueue": "true"}'
+awslocal sqs create-queue --queue-name notification-queue.fifo --attributes '{"FifoQueue": "true"}'
+awslocal sqs create-queue --queue-name subscription-request-queue.fifo --attributes '{"FifoQueue": "true"}'
 
 echo "AWS LocalStack resources initialized successfully!"
