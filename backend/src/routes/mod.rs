149db15d6c58698b7a25e0257733745a3217986d
Takis Kakalis <80459599+Takaros999@users.noreply.github.com>
2025-08-04 16:19:00 +0200

    Add health endpoint

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -1,11 +1,13 @@
 mod docs;
+mod health;
 pub mod v1;
 
-use aide::axum::ApiRouter;
+use aide::axum::{routing::get, ApiRouter};
 
 /// Creates the router with all handler routes
 pub fn handler() -> ApiRouter {
     ApiRouter::new()
         .merge(docs::handler())
+        .api_route("/health", get(health::handler))
         .nest("/v1", v1::handler())
 }
