6933b38ecedfd1e954077bb46a55fdc40442c98e
Takis Kakalis <80459599+Takaros999@users.noreply.github.com>
2025-09-12 17:43:38 +0200

    v1 route

--- enclave-worker/src/routes/mod.rs
+++ enclave-worker/src/routes/mod.rs
@@ -12,5 +12,5 @@ pub fn handler() -> ApiRouter {
     ApiRouter::new()
         .merge(docs::handler())
         .api_route("/health", get(health::handler))
-        .api_route("/push-id-challenge", post(push_id_challenge::handler))
+        .api_route("/v1/push-id-challenge", post(push_id_challenge::handler))
 }
