22029cb4dff714656ed068eed13900ef0f698605
Takis Kakalis <80459599+Takaros999@users.noreply.github.com>
2025-09-12 17:04:14 +0200

    Add types

--- shared/enclave-types/src/lib.rs
+++ shared/enclave-types/src/lib.rs
@@ -1,41 +1,50 @@
-use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use pontifex::Request;
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
 
-/// Main request type sent from the enclave-worker to the secure-enclave
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum EnclaveRequest {
-    /// Request to initialize the enclave with Braze configuration
-    Initialize(EnclaveConfig),
-    /// Health check request
-    HealthCheck,
-}
-
-/// Response type sent from the secure-enclave back to the enclave-worker
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum EnclaveResponse {
-    /// Initialization successful
-    InitializeSuccess,
-    /// Health check response
-    HealthCheck { initialized: bool },
-    /// Error response
-    Error(EnclaveError),
-}
-
-/// Braze API configuration
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct EnclaveConfig {
-    /// Braze API key
-    pub braze_api_key: String,
-    /// Braze REST endpoint URL (e.g., https://rest.iad-01.braze.com)
-    pub braze_api_endpoint: String,
-    /// Enclave HTTP proxy port
-    pub braze_http_proxy_port: u32,
-}
-
-/// Enclave errors
-#[derive(Debug, Clone, Serialize, Deserialize, thiserror::Error)]
+#[derive(Debug, Clone, Serialize, Deserialize, Error)]
 pub enum EnclaveError {
     #[error("Enclave not initialized. Call Initialize first.")]
     NotInitialized,
     #[error("Unexpected response type")]
     UnexpectedResponse,
+}
+
+/// Braze API configuration
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct EnclaveInitializeRequest {
+    /// Braze API key
+    pub braze_api_key: String,
+    /// Braze REST endpoint URL (e.g., https://rest.iad-01.braze.com)
+    pub braze_http_proxy_port: u32,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct EnclaveInitializeResponse {
+    /// Success message
+    pub success: bool,
+}
+
+impl Request for EnclaveInitializeRequest {
+    const ROUTE_ID: &'static str = "/v1/initialize";
+    type Response = Result<EnclaveInitializeResponse, EnclaveError>;
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct EnclaveHealthCheckRequest;
+
+impl Request for EnclaveHealthCheckRequest {
+    const ROUTE_ID: &'static str = "/v1/health-check";
+    type Response = Result<(), EnclaveError>;
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct EnclavePushIdChallengeRequest {
+    pub encrypted_push_id_1: String,
+    pub encrypted_push_id_2: String,
+}
+
+impl Request for EnclavePushIdChallengeRequest {
+    const ROUTE_ID: &'static str = "/v1/push-id-challenge";
+    type Response = Result<bool, EnclaveError>;
 }
