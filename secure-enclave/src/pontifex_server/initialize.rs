19c59466875ba913ca808911e6b11cc487968f24
Takis Kakalis <80459599+Takaros999@users.noreply.github.com>
2025-09-28 12:33:41 +0200

    feat: Move braze logic inside the enclave (#70)
    * Secure enclave notif logic
    
    * complete flow
    
    * Make it work with http2
    
    * Update .env.example
    
    * Switch to ecr gallery
    
    * add info log
    
    * break infinite loop
    
    * Remove panic and old comment
    
    * Revert docker image change and set published pontifex version
    
    * Proto
    
    * cargo.lock
    
    * Update CI
    
    * clippy and cargo

--- secure-enclave/src/pontifex_server/initialize.rs
+++ secure-enclave/src/pontifex_server/initialize.rs
@@ -1,26 +1,24 @@
 use std::sync::Arc;
 
 use crate::state::EnclaveState;
-use enclave_types::{EnclaveError, EnclaveInitializeRequest, EnclaveInitializeResponse};
+use enclave_types::EnclaveInitializeRequest;
 use tokio::sync::RwLock;
 use tracing::info;
 
-pub async fn handler(
-    state: Arc<RwLock<EnclaveState>>,
-    config: EnclaveInitializeRequest,
-) -> Result<EnclaveInitializeResponse, EnclaveError> {
-    let client = pontifex::http::client(config.braze_http_proxy_port);
+pub async fn handler(state: Arc<RwLock<EnclaveState>>, config: EnclaveInitializeRequest) {
+    let client = pontifex::http::client_http2_only(
+        config.braze_http_proxy_port,
+        &pontifex::http::Http2ClientConfig::default(),
+    );
 
     let mut state = state.write().await;
     state.http_proxy_client = Some(client);
     state.braze_api_key = Some(config.braze_api_key);
-    state.braze_api_endpoint = Some(format!(
+    state.braze_api_url = Some(format!(
         "https://rest.{}.braze.com",
         config.braze_api_region
     ));
     state.initialized = true;
 
     info!("✅ Enclave initialized successfully");
-
-    Ok(EnclaveInitializeResponse { success: true })
 }
--- secure-enclave/src/pontifex_server/mod.rs
+++ secure-enclave/src/pontifex_server/mod.rs
@@ -3,11 +3,14 @@ use std::sync::Arc;
 use anyhow::Context;
 use enclave_types::{
     EnclaveAttestationDocRequest, EnclaveHealthCheckRequest, EnclaveInitializeRequest,
+    EnclaveNotificationRequest,
 };
 use pontifex::Router;
 use tokio::sync::RwLock;
+
 mod health;
 mod initialize;
+mod notification;
 mod public_key;
 
 use crate::state::EnclaveState;
@@ -20,7 +23,8 @@ pub async fn start_pontifex_server(
     let router = Router::with_state(state)
         .route::<EnclaveInitializeRequest, _, _>(initialize::handler)
         .route::<EnclaveHealthCheckRequest, _, _>(health::handler)
-        .route::<EnclaveAttestationDocRequest, _, _>(public_key::handler);
+        .route::<EnclaveAttestationDocRequest, _, _>(public_key::handler)
+        .route::<EnclaveNotificationRequest, _, _>(notification::handler);
 
     // Start pontifex server
     router
