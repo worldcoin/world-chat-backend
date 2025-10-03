5c75a2aa815a2a9fe0101d99b0ebb49cc175bce7
Takis Kakalis <80459599+Takaros999@users.noreply.github.com>
2025-10-03 13:19:26 +0200

    feat: Use Braze user aliases (#87)
    * feat: Use Braze user aliases
    
    * proto

--- secure-enclave/src/pontifex_server/notification.rs
+++ secure-enclave/src/pontifex_server/notification.rs
@@ -1,9 +1,11 @@
 use std::sync::Arc;
 
 use crate::state::EnclaveState;
+use crypto_box::SecretKey;
 use enclave_types::{EnclaveError, EnclaveNotificationRequest};
 use hyper::{Body, Method, Request, Version};
 use pontifex::http::HttpClient;
+use serde::Serialize;
 use serde_json::json;
 use tokio::sync::RwLock;
 
@@ -12,6 +14,7 @@ pub async fn handler(
     request: EnclaveNotificationRequest,
 ) -> Result<(), EnclaveError> {
     let state = state.read().await;
+    let encryption_key = state.keys.private_key.clone();
 
     if !state.initialized {
         return Err(EnclaveError::NotInitialized);
@@ -22,12 +25,18 @@ pub async fn handler(
     let braze_api_endpoint = state.braze_api_url.clone().unwrap();
     let braze_api_endpoint = format!("{braze_api_endpoint}/messages/send");
 
+    let user_aliases = request
+        .subscribed_encrypted_push_ids
+        .iter()
+        .map(|id| decrypt_push_id_and_create_alias(id.clone(), &encryption_key))
+        .collect::<Result<Vec<UserAlias>, EnclaveError>>()?;
+
     send_braze_notification(
         client,
         braze_api_key,
         braze_api_endpoint,
         request.topic,
-        request.subscribed_encrypted_push_ids,
+        user_aliases,
         request.encrypted_message_base64,
     )
     .await?;
@@ -35,17 +44,47 @@ pub async fn handler(
     Ok(())
 }
 
+fn decrypt_push_id_and_create_alias(
+    encrypted_push_id: String,
+    encryption_key: &SecretKey,
+) -> Result<UserAlias, EnclaveError> {
+    let encrypted_push_id = hex::decode(encrypted_push_id)
+        .map_err(|e| EnclaveError::BrazeRequestFailed(format!("Hex decode failed: {e:?}")))?;
+
+    let push_id = encryption_key
+        .unseal(&encrypted_push_id)
+        .map(|decrypted| hex::encode(decrypted))
+        .map_err(|e| EnclaveError::BrazeRequestFailed(format!("Unseal failed: {e:?}")))?;
+
+    Ok(UserAlias::push_id_alias(push_id))
+}
+
+#[derive(Serialize)]
+struct UserAlias {
+    alias_name: String,
+    alias_label: String,
+}
+
+impl UserAlias {
+    #[must_use]
+    pub fn push_id_alias(push_id: String) -> Self {
+        Self {
+            alias_name: push_id,
+            alias_label: "push_id".to_string(),
+        }
+    }
+}
+
 async fn send_braze_notification(
     client: &HttpClient,
     braze_api_key: String,
     braze_api_endpoint: String,
     topic: String,
-    subscribed_encrypted_push_ids: Vec<String>,
+    user_aliases: Vec<UserAlias>,
     encrypted_message_base64: String,
 ) -> Result<(), EnclaveError> {
     let body = json!({
-        // TODO: Decrypt push IDs and use Braze user aliases instead
-        "external_user_ids": subscribed_encrypted_push_ids,
+        "user_aliases": user_aliases,
         "messages": {
             "apple_push": {
                 "alert": {
@@ -82,12 +121,12 @@ async fn send_braze_notification(
         .header("Authorization", format!("Bearer {}", braze_api_key))
         .header("Content-Type", "application/json")
         .body(body)
-        .map_err(|e| EnclaveError::BrazeRequestFailed(format!("{e:?}")))?;
+        .map_err(|e| EnclaveError::BrazeRequestFailed(format!("Request builder failed: {e:?}")))?;
 
     client
         .request(req)
         .await
-        .map_err(|e| EnclaveError::BrazeRequestFailed(format!("{e:?}")))?;
+        .map_err(|e| EnclaveError::BrazeRequestFailed(format!("Braze request failed: {e:?}")))?;
 
     Ok(())
 }
