f3142fd2b13dee1095e51ed898c9f76661476ab9
Takis Kakalis <80459599+Takaros999@users.noreply.github.com>
2025-11-12 12:18:52 -0300

    feat: Add logging for attestation caching (#112)
    * feat: Add logging for attestation caching
    
    * Flaky test

--- enclave-worker/src/routes/attestation.rs
+++ enclave-worker/src/routes/attestation.rs
@@ -4,12 +4,13 @@ use base64::engine::general_purpose::STANDARD;
 use base64::Engine;
 use common_types::AttestationDocumentResponse;
 use enclave_types::EnclaveAttestationDocRequest;
+use tracing::info;
 
 use crate::cache::CacheManager;
 use crate::types::AppError;
 
-const EXPIRATION_TIME: u64 = 60 * 60 * 3; // 3 hours
-const REFRESH_THRESHOLD: u64 = 60 * 30; // 30 minutes before expiration
+const EXPIRATION_TIME_SECS: u64 = 60 * 60 * 3; // 3 hours
+const REFRESH_THRESHOLD_SECS: u64 = 60 * 30; // 30 minutes before expiration
 const CACHE_KEY: &str = "enclave-worker:attestation-document";
 
 pub async fn handler(
@@ -19,8 +20,8 @@ pub async fn handler(
     let attestation_doc = cache_manager
         .cache_with_refresh(
             CACHE_KEY,
-            EXPIRATION_TIME,
-            REFRESH_THRESHOLD,
+            EXPIRATION_TIME_SECS,
+            REFRESH_THRESHOLD_SECS,
             move || async move {
                 let request = EnclaveAttestationDocRequest {};
                 let response = pontifex::client::send::<EnclaveAttestationDocRequest>(
@@ -30,6 +31,8 @@ pub async fn handler(
                 .await
                 .context("Pontifex error")?
                 .context("Failed to fetch attestation document")?;
+
+                info!(attestation = %STANDARD.encode(response.attestation.clone()), "Refreshed attestation document");
 
                 Ok(response.attestation)
             },
