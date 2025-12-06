ceb5b3121f2373a0f1e719be42dc65fc9afab8d5
Takis Kakalis <80459599+Takaros999@users.noreply.github.com>
2025-12-05 17:10:58 -0800

    comment

--- backend/src/routes/v1/media.rs
+++ backend/src/routes/v1/media.rs
@@ -53,7 +53,8 @@ where
         ));
     }
 
-    if matches!(m.type_(), mime::IMAGE | mime::VIDEO) {
+    // Allow only image/* and video/* and application/octet-stream because images will be encrypted blobs
+    if matches!(m.type_(), mime::IMAGE | mime::VIDEO) || m == mime::APPLICATION_OCTET_STREAM {
         Ok(m)
     } else {
         Err(serde::de::Error::custom("mime must be image/* or video/*"))
